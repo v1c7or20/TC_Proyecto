a7b7cd0105608d652e320af31a89c6fa5a545512
v1ct0r20 <victor.pena@utec.edu.pe>
2020-01-29 10:42:32 -0500

    Part 0.1

--- dictionary.hpp
+++ dictionary.hpp
@@ -2,39 +2,48 @@
 #include <iostream>
 #include <unordered_map>
 #include <algorithm>
+#include <vector>
+
 
 using namespace std;
 
 
-class dictionary
- {
+class dictionary{
+ private:
+ 	vector<string> dict;
  public:
  	dictionary();
  	~dictionary();
- 	bool find(string toLook):
+ 	bool find(string toLook);
 
- private:
- 	std::vector<string> disct;
  };
 
  dictionary::dictionary(){
- 	disct.push_back("ein");
- 	disct.push_back("zwei");
- 	disct.push_back("drei");
- 	disct.push_back("vier");
- 	disct.push_back("fünf");
- 	disct.push_back("sechs");
- 	disct.push_back("sieben");
- 	disct.push_back("acht");
- 	disct.push_back("neun");
- 	disct.push_back("zehn");
- 	disct.push_back("elf");
- 	disct.push_back("zwölf");
- 	disct.push_back("sech");
- 	disct.push_back("sieb");
- 	disct.push_back("swan");
- 	disct.push_back("hundert");
- 	disct.push_back("tausend");
- 	disct.push_back("zig");
- 	disct.push_back("ssig");
+ 	dict.push_back("ein");
+ 	dict.push_back("zwei");
+ 	dict.push_back("drei");
+ 	dict.push_back("vier");
+ 	dict.push_back("fünf");
+ 	dict.push_back("sechs");
+ 	dict.push_back("sieben");
+ 	dict.push_back("acht");
+ 	dict.push_back("neun");
+ 	dict.push_back("zehn");
+ 	dict.push_back("elf");
+ 	dict.push_back("zwölf");
+ 	dict.push_back("sech");
+ 	dict.push_back("sieb");
+ 	dict.push_back("swan");
+ 	dict.push_back("hundert");
+ 	dict.push_back("tausend");
+ 	dict.push_back("zig");
+ 	dict.push_back("ssig");
+ }
+
+ bool dictionary::find(string toLook){
+ 	for(auto iter = dict.begin(); iter != dict.end(); iter++){
+ 		if (toLook == *iter){
+ 			return true;
+ 		}
+ 	}
  }
--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 #include <iterator>
-
+#include "dictionary.hpp"
 using namespace std;
 
 int main(int argc, char const *argv[])
