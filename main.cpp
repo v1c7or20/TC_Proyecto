fb3d3b1e6b0172f7316187ca9b11bd1c0f9cd4c2
victor <victor.pena@utec.edu.oe>
2020-02-02 15:40:06 -0500

    Done

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include "parser.h"
-#include "dictionary.h"
+#include "functions.cpp"
 
 using namespace std;
 
 int main(int argc, char const *argv[]){
     parser *Parser = new parser();
     string toTranslate("The father of the mother of the mother of the father of John");
-    cout<<Parser->parse(toTranslate);
+    cout<<Parser->parse(toTranslate)<<endl;
+    cout<<middleInterpretation("greatgreatgreatgrandmother");
 	return 0;
 }
