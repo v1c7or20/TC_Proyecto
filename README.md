cd40712b60c4e80c2902716615ffbd3e929786c3
v1ct0r20 <victor.pena@utec.edu.pe>
2020-02-08 23:41:44 -0500

    Avance

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # TC_Proyecto
 
 #1 Gramatica
-    Sentence  -> The mother Sentence2 
-    Sentence2 -> of the mother Sentence2 | of Nombre 
-    Nombre    -> Mary | John
+    Sentence    -> Init Sentence2
+    Sentence2   -> Second Sentence2 | Second Sentence2 | O Nombre 
+    Nombre      -> Mary | John
+    Init        -> The mother | The father 
+    Second      -> of the mother | of the father 
+    O           -> of
--- main.cpp
+++ main.cpp
@@ -8,6 +8,10 @@ int main(int argc, char const *argv[]){
     parser *Parser = new parser();
     string toTranslate("The father of the mother of the mother of the father of John");
     cout<<Parser->parse(toTranslate)<<endl;
-    cout<<middleInterpretation("greatgreatgreatgrandmother");
-	return 0;
+    cout<<middleInterpretation("greatgreatgreatgrandmother")<<endl;
+    cout<<middleInterpretation("grandgreatgreatgrandmother")<<endl;
+    cout<<finalInterpretation(middleInterpretation("greatgreatgreatgrandmother"))<<endl;
+    cout<<finalInterpretation("g(g(fa(g(mo()))))")<<endl;
+
+    return 0;
 }
